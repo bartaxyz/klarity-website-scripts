d852da4d9af65347527092c75bb9a935d645202b
bartaxyz <bartaxyz@users.noreply.github.com>
2022-01-07 15:22:31 +0000

    deploy: 80315b87b43c401ad215230072de3ae417779ad1

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-(()=>{var e=class{constructor(o){}};e.className="any";var s=class extends e{constructor(o){super(o);console.log("Feedback Carousel"),console.log(o)}};s.className="feedback-carousel";})();
+(()=>{var e=class{constructor(o){}};e.data="any";var t=class extends e{constructor(o){super(o);console.log("Feedback Carousel"),console.log(o)}};t.data="feedback-carousel";})();
 //# sourceMappingURL=index.js.map
